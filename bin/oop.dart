73152ae7f746a1de2c8fa59a9227cf80ad265250
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2023-10-18 13:39:59 +0600

    commit dart

--- bin/oop.dart
+++ bin/oop.dart
@@ -1,13 +1,10 @@
 List<String> capitalize(String x) {
-  List even=[];
-  List odd=[];
-  
-  
-   for (var i=0; i<x.length; i++){
-    even.add(i%2==0 ? x [i].toUpperCase():x[i]);
-    odd.add(i%2==1 ? x [i].toUpperCase():x[i]); 
-   }
-   return [even.join(), odd.join()];
+  List even = [];
+  List odd = [];
 
-
+  for (var i = 0; i < x.length; i++) {
+    even.add(i % 2 == 0 ? x[i].toUpperCase() : x[i]);
+    odd.add(i % 2 == 1 ? x[i].toUpperCase() : x[i]);
+  }
+  return [even.join(), odd.join()];
 }
--- bin/ternat.dart
+++
@@ -1,7 +0,0 @@
-// condition ? expression1 : expression2
-
-void main() {
-  int age = 20;
-  String result = age >= 18 ? "Совершеннолетний" : "Несовершеннолетний";
-  print("Возраст: $age, статус: $result");
-}
