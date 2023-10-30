7144071bba69bddb46b1d3bd1cd52a725883895e
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2023-10-30 22:20:15 +0600

    oop4

--- bin/oop4.dart
+++ bin/oop4.dart
@@ -4,31 +4,22 @@ void main(List<String> arguments) {
   print('Hello world: ${oop4.calculate()}!');
 }
 
-
-
-
-
-class Car {
-
-
-  Car( 
+class Mashina {
+  Mashina(
     this.name,
-    this.year, this.obem,
+    this.year,
+    this.obem,
     this.country,
     this.price,
-    this.id, this.model ,
+    this.id,
+    this.model,
   );
 
-
-
- final String name ;
- final String model;
- final int year;
- final String obem;
- final String country;
- final int price;
- final int id;
-
+  final String name;
+  final String model;
+  final int year;
+  final String obem;
+  final String country;
+  final int price;
+  final int id;
 }
-
-
