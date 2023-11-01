2d817389c3bc515eb7755d0109a1fc753fed9041
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2023-11-01 09:04:06 +0600

    try catch commit

--- bin/http.dart
+++ bin/http.dart
@@ -1,21 +1,10 @@
 import 'package:http/http.dart';
 
 void main(List<String> args) async {
-  // const url = "https://pub.dev/packages/http";
-  // final uri = Uri.parse(url);
-  // final client = Client();
-  // final response = await client.get(uri);
-  // print(response);
-  // print(response.body);
-
-  try {
-    const url = "https://pub.dev/packages/htt";
-    final uri = Uri.parse(url);
-    final client = Client();
-    final response = await client.get(uri);
-    print(response);
-    print(response.body);
-  } catch (e) {
-    print("Ссылке есть ощибка");
-  }
+  const url = "https://pub.dev/packages/http";
+  final uri = Uri.parse(url);
+  final client = Client();
+  final response = await client.get(uri);
+  print(response);
+  print(response.body);
 }
---
+++ bin/trycatch.dart
@@ -0,0 +1,46 @@
+// import 'package:http/http.dart';
+
+// 1-exemple
+// void main(List<String> args) async {
+//   try {
+//     const url = "https://pub.dev/packages/htt";
+//     final uri = Uri.parse(url);
+//     final client = Client();
+//     final response = await client.get(uri);
+//     print(response);
+//     print(response.body);
+//   } catch (e) {
+//     print("Ссылке есть ощибка");
+//   }
+// }
+
+// 2-exemple
+
+// void main() {
+//   try {
+//     int x = 9;
+//     int y = 0;
+//     int z = x ~/ y;
+
+//     print(z);
+//   } catch (e) {
+//     print("Возникло исключение $e");
+//   }
+//   print("Завершение программы");
+// }
+
+// 3-exemple
+
+// void main() {
+//   try {
+//     catchMeIfYouCan();
+//   } catch (e) {
+//     print(e.toString());
+//   }
+// }
+
+// Future<void> catchMeIfYouCan() async {
+//   await Future.delayed(Duration(seconds: 4), () {
+//     throw Exception('did you?');
+//   });
+// }
