3bf45fc65aa281904f72db21b460880e211eb588
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2023-11-27 23:55:26 +0600

    added complex_constructor.dart file

--- bin/factory_constructor.dart
+++ bin/factory_constructor.dart
@@ -1,58 +1,19 @@
-// 1)бул жонокой конструктор
-// User(
-//     this.name,
-//     this.finalName,
-//     this.age,
-//     this.nation,
-//     this.profession,
-//     this.tuulganJer,
-//     this.password,
-//   );
+// Factory Constructor-Фабричный конструктор
+// hashcode сравнение
+// Метод hashCode расчитывает и возвращает хэш-код данного объекта
 class User {
-  // 1.1)Defaut constructor болсо
-// дефолту бар мисалы эгерде бербесе ушул деп кетет.мисалы биздин Userибиз нация деген жерди жазбаса по умолчанию кыргыз деп кетсин
-  // User(
-  //   this.name,
-  //   this.finalName,
-  //   this.age,
-  //   this.profession,
-  //   this.password, [
-  //   this.nation = 'Kyrgyz', //здесь по умолчанию будет кыргыз
-  //   this.tuulganJer =
-  //       'Kyrgyzstan', //tuulganJer будет тоже по умолчанию Kyrgyzstan
-  // ]);
-// 2) Named constructor мындайча айтканда атама конструктор агрументин берерде атап беришибиз керек
-// мисалы жонокойдо мындай берсек final user1 = User('Dastan', 'Almazbekov', 20, 'Developer', 'qwerty');
-// Named constructor атап беребиз мисалы final user2=User(name: 'Dastan', finalName: 'Almazbekov', age: 20,
-// бул эмне учун керек, Атаганда кезегине карабасак да болок
-// Named constructorдо дагы Defaut бар аны required очуруп this.nation='kyrgyz', деп коебуз
-  User({
-    required this.name,
-    required this.finalName,
-    required this.age,
-    this.nation = 'kyrgyz',
-    required this.profession,
-    required this.tuulganJer,
-    required this.password,
-  });
+  int id;
 
-  final String name;
-  final String finalName;
-  final int age;
-  final String nation;
-  final String profession;
-  final String tuulganJer;
-  final String password;
+  User(this.id);
 }
 
+final a = User(0);
+final b = User(0);
+// одиноковые хеш код
+// String a = 'Hello';
+// String b = 'Hello';
+
 void main() {
-  // final user1 = User('Dastan', 'Almazbekov', 20, 'Developer', 'qwerty');
-  // final user2 = User(
-  //     name: 'Dastan',
-  //     finalName: 'Almazbekov',
-  //     age: 20,
-  //     nation: 'Kyrgyz',
-  //     profession: 'Developer',
-  //     tuulganJer: 'Kyrgyzstan',
-  //     password: 'qwerty');
+  print(a.hashCode);
+  print(b.hashCode);
 }
